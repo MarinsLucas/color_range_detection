646d8e4ddad75b00a5b28415dd5e68e036f5273b
Lucas Marins <lucasmarinsramalho@gmail.com>
2023-02-16 19:55:36 -0300

    commit for testing in rinobt pc

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,5 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
+#include <iostream>
+#include <fstream> 
 #include<vector>
 using namespace cv;
 using namespace std; 
@@ -7,199 +9,368 @@ using namespace std;
 //BGR2HSV
 //BGR2LAB
 
-#define HSV_ 0
-#define LAB_ 1
+#define HSV_ 1
+#define LAB_ 0
 
-#define piracicaba 2
+Mat rawFrame;
+VideoCapture cap; 
+int B, A, L, H, S, V; //aux variables
+string current_color_name;
+string* possibilities = {"Amarelo", "Verde", "Azul", "Ciano", "Laranja", "Rosa", "Marrom", "Vermelho", "Roxo", "ColorEnemy1", "ColorEnemy2", "ColorEnemy3"};
 
-Mat src;
-char window_name[4] = "aaa";
-int B, A, L, H, S, V;
-int color_space;
+void writeFile(int* min, int* max, int cs, int c)
+{
+  string straux; 
+  fstream file.open("../RinobotSystem/config/colors");
+  int index = -1;
+  while(!file.eof())
+    {
+      getline(file, straux);
+      if(straux.size() > 4)
+      {
+        index++;
+      }
+      if(straux == current_color_name)
+      {
+        file<<min[0]<<endl<<max[0]<<endl<<min[1]<<endl<<max[1]<<endl<<min[2]<<endl<<max[2];
+      }
+    }
+  file.close();
 
-static void onMouse(int event, int x, int y, int f, void* vet)
+  fstream color_space_file.open("../RinobotSystem/config/typeColors");
+  for(int i =0; i<index; i++)
+    {
+      getline(color_space_file, straux);
+    }
+  color_space_file<<cs;
+  color_space_file.close();
+}
+
+static void onMouseHSV(int event, int x, int y, int f, void* vet)
 {
 	vector<Vec3b>* vet_colors = (vector<Vec3b>*) vet;
-	Mat image = src.clone();
-	Vec3b rgb; 
-	Mat Lab;
-	Mat RGB; 
-	Vec3b hsv;
-	Vec3b lab;
-	Mat HSV;
+  Mat HSVmat;
+  Mat RBGmat;
+  Vec3b hsv3b;
 
-	if(event == EVENT_LBUTTONDOWN)
+  if(event == EVENT_LBUTTONDOWN)
+  {
+		RGB=rawFrame(Rect(x,y,1, 1));
+    cvtColor(RGB, HSVmat, COLOR_BGR2HSV); 
+    hsv3b = HSVmat.at<Vec3b>(0, 0);
+    H = hsv3b.val[0];
+    S = hsv3b.val[1];
+    V = hsv3b.val[2];
+    vet_colors->push_back(hsv3b);
+  }
+
+  if(event == EVENT_RBUTTONDOWN)
 	{
-		rgb = image.at<Vec3b>(y,x);
-
-		RGB=image(Rect(x,y,1, 1));
-
-		if(color_space == LAB_)
-		{
-			cvtColor(RGB, Lab, COLOR_BGR2Lab); 
-			lab = Lab.at<Vec3b>(0, 0);
-			L = lab.val[0];
-			A = lab.val[1];
-			B = lab.val[2];
-			vet_colors->push_back(lab);
-		}
-		if(color_space == HSV_)
-		{
-			cvtColor(RGB, HSV, COLOR_BGR2HSV); 
-			hsv = HSV.at<Vec3b>(0, 0);
-			H = hsv.val[0];
-			S = hsv.val[1];
-			V = hsv.val[2];
-			vet_colors->push_back(hsv);
-		}
-                           
-	}
-
-	if(event == EVENT_RBUTTONDOWN)
-	{
-		int max_[3];
+    int max_[3];
 		int min_[3];
 		int min = 0;
 		int max = 256; 
-		if(color_space == HSV_)
-		{
-			cout<<"HSV Colors: "<<endl;
-			for(int i = 0; i<vet_colors->size(); i++)
-			{
-				cout<<vet_colors->at(i)<<" ";
-			}
-			cout<<endl;
-			cout<<"Range HSV colors"<<endl;
-			for(int j = 0; j<3; j++)
-			{
-				min = 256; max = -1;
-				for(int i = 0; i<vet_colors->size(); i++)
-				{
-					if(vet_colors->at(i).val[j] < min)
-					{
-						min = vet_colors->at(i).val[j];
-					}
 
-					if(vet_colors->at(i).val[j] > max)
-					{
-						max = vet_colors->at(i).val[j];
-					}
-				}
+    //Debug
+    /*cout<<"HSV Colors: "<<endl;
+    for(int i = 0; i<vet_colors->size(); i++)
+    {
+      cout<<vet_colors->at(i)<<" ";
+    }
+    cout<<endl;*/
+    
+    //cout<<"Range HSV colors"<<endl;
+    for(int j = 0; j<3; j++)
+    {
+      min = 256; max = -1;
+      for(int i = 0; i<vet_colors->size(); i++)
+      {
+        if(vet_colors->at(i).val[j] < min)
+        {
+          min = vet_colors->at(i).val[j];
+        }
 
-				cout<<j<<" - max: " << max << " min: " <<min<<endl;
-				max_[j] = max;
-				min_[j] = min;
-			}
-		}
+        if(vet_colors->at(i).val[j] > max)
+        {
+          max = vet_colors->at(i).val[j];
+        }
+      }
 
-		else if(color_space == LAB_)
-		{
-			cout<<endl<<"Lab Colors: "<<endl;
-			for(int i = 0; i<vet_colors->size(); i++)
-			{
-				cout<<vet_colors->at(i)<<" ";
-			}
-			cout<<endl; 
+      //cout<<j<<" - max: " << max << " min: " <<min<<endl;
+      max_[j] = max;
+      min_[j] = min;
+  } 
+  //end debug
 
-			cout<<"Range Lab colors"<<endl;
-			for(int j = 0; j<3; j++)
-			{
-				min = 256; max = -1;
-				for(int i = 0; i<vet_colors->size(); i++)
-				{
-					if(vet_colors->at(i).val[j] < min)
-					{
-						min = vet_colors->at(i).val[j];
-					}
+  writeFile(min, max, HSV_, 3);
+    
+  Mat nimage;
+  cvtColor(rawFrame, nimage, COLOR_BGR2HSV);
 
-					if(vet_colors->at(i).val[j] > max)
-					{
-						max = vet_colors->at(i).val[j];
-					}
-				}
+  for(int i = 0; i<nimage.cols; i++)
+  {
+    for(int j =0; j<nimage.rows; j++)
+    {
+        Vec3b col = nimage.at<Vec3b>(j, i);
 
-				cout<<j<<" - max: " << max << " min: " <<min<<endl;
-				max_[j] = max;
-				min_[j] = min;
-			}
-		}
+        if(col.val[0] < min_[0] || col.val[0] > max_[0])
+        {
+          nimage.at<Vec3b>(j,i).val[0] = 0;
+          nimage.at<Vec3b>(j,i).val[1] = 0;
+          nimage.at<Vec3b>(j,i).val[2] = 0;
+        }
+        if(col.val[1]< min_[1] || col.val[1] > max_[1])
+        {
+          nimage.at<Vec3b>(j,i).val[0] = 0;
+          nimage.at<Vec3b>(j,i).val[1] = 0;
+          nimage.at<Vec3b>(j,i).val[2] = 0;
 
-		Mat nimage;
-		cvtColor(image, nimage, COLOR_BGR2Lab);
-		// image = src.clone(); 
+        }
+        if(col.val[2] < min_[2]|| col.val[2] > max_[2)
+        {
+          nimage.at<Vec3b>(j,i).val[0] = 0;
+          nimage.at<Vec3b>(j,i).val[1] = 0;
+          nimage.at<Vec3b>(j,i).val[2] = 0;
+        }
 
-		for(int i = 0; i<nimage.cols; i++)
-		{
-			for(int j =0; j<nimage.rows; j++)
-			{
-					Vec3b col = nimage.at<Vec3b>(j, i);
+    }
+  }
+  imshow(window_name, nimage);
 
-					if(col.val[0] < min_[0] - piracicaba || col.val[0] > max_[0] + piracicaba)
-					{
-						nimage.at<Vec3b>(j,i).val[0] = 0;
-						nimage.at<Vec3b>(j,i).val[1] = 0;
-						nimage.at<Vec3b>(j,i).val[2] = 0;
-					}
-					if(col.val[1]< min_[1] - piracicaba || col.val[1] > max_[1] + piracicaba)
-					{
-						nimage.at<Vec3b>(j,i).val[0] = 0;
-						nimage.at<Vec3b>(j,i).val[1] = 0;
-						nimage.at<Vec3b>(j,i).val[2] = 0;
+}
+  else
+  {
+    imshow(window_name, rawFrame);
+  }
+  
+}
 
-					}
-					if(col.val[2] < min_[2] - piracicaba|| col.val[2] > max_[2]+ piracicaba)
-					{
-						nimage.at<Vec3b>(j,i).val[0] = 0;
-						nimage.at<Vec3b>(j,i).val[1] = 0;
-						nimage.at<Vec3b>(j,i).val[2] = 0;
-					}
+static void onMouseLab(int event, int x, int y, int f, void* vet)
+{
+  vector<Vec3b>* vet_colors = (vector<Vec3b>*) vet;
+  Mat LAB;
+  Mat RGB;
+  Vec3b lab3b;
 
-			}
-		}
+  if(event == EVENT_LBUTTONDOWN)
+  {
+		RGB=rawFrame(Rect(x,y,1, 1));
+    cvtColor(RGB, Lab, COLOR_BGR2Lab); 
+    lab3b = Lab.at<Vec3b>(0, 0);
+    L = lab3b.val[0];
+    A = lab3b.val[1];
+    B = lab3b.val[2];
+    vet_colors->push_back(lab3b);
+  }
 
-		cvtColor(nimage, image, COLOR_Lab2BGR);
-		src = image.clone();
-	}
+  if(event == EVENT_RBUTTONDOWN)
+	{
+    int max_[3];
+		int min_[3];
+		int min = 0;
+		int max = 256; 
 
-	char name[30];
-	sprintf(name, "L=%d", L);
-	putText(image, name, Point(150, 40), FONT_HERSHEY_SIMPLEX, .7, Scalar(0, 255, 0), 2, 8, false);
-	sprintf(name, "a=%d", A);
-	putText(image, name, Point(250, 40), FONT_HERSHEY_SIMPLEX, .7, Scalar(0, 255, 0), 2, 8, false);
-	sprintf(name, "b=%d", B);
-	putText(image, name, Point(350, 40), FONT_HERSHEY_SIMPLEX, .7, Scalar(0, 255, 0), 2, 8, false);
-	sprintf(name, "H=%d", H);
-	putText(image, name, Point(450, 40), FONT_HERSHEY_SIMPLEX, .7, Scalar(0, 255, 0), 2, 8, false);
-	sprintf(name, "S=%d", S);
-	putText(image, name, Point(550, 40), FONT_HERSHEY_SIMPLEX, .7, Scalar(0, 255, 0), 2, 8, false);
-	sprintf(name, "V=%d", V);
-	putText(image, name, Point(650, 40), FONT_HERSHEY_SIMPLEX, .7, Scalar(0, 255, 0), 2, 8, false);
+    //Debug
+    /*cout<<"Lab Colors: "<<endl;
+    for(int i = 0; i<vet_colors->size(); i++)
+    {
+      cout<<vet_colors->at(i)<<" ";
+    }
+    cout<<endl;*/
+    
+    //cout<<"Range Lab colors"<<endl;
+    for(int j = 0; j<3; j++)
+    {
+      min = 256; max = -1;
+      for(int i = 0; i<vet_colors->size(); i++)
+      {
+        if(vet_colors->at(i).val[j] < min)
+        {
+          min = vet_colors->at(i).val[j];
+        }
 
-	imshow(window_name, image);
+        if(vet_colors->at(i).val[j] > max)
+        {
+          max = vet_colors->at(i).val[j];
+        }
+      }
+
+      //cout<<j<<" - max: " << max << " min: " <<min<<endl;
+      max_[j] = max;
+      min_[j] = min;
+  } 
+  //end debug
+
+  writeFile(min, max, LAB_, 3);
+
+  Mat nimage;
+  cvtColor(rawFrame, nimage, COLOR_BGR2Lab);
+
+  for(int i = 0; i<nimage.cols; i++)
+  {
+    for(int j =0; j<nimage.rows; j++)
+    {
+        Vec3b col = nimage.at<Vec3b>(j, i);
+
+        if(col.val[0] < min_[0] || col.val[0] > max_[0])
+        {
+          nimage.at<Vec3b>(j,i).val[0] = 0;
+          nimage.at<Vec3b>(j,i).val[1] = 0;
+          nimage.at<Vec3b>(j,i).val[2] = 0;
+        }
+        if(col.val[1]< min_[1] || col.val[1] > max_[1])
+        {
+          nimage.at<Vec3b>(j,i).val[0] = 0;
+          nimage.at<Vec3b>(j,i).val[1] = 0;
+          nimage.at<Vec3b>(j,i).val[2] = 0;
+
+        }
+        if(col.val[2] < min_[2]|| col.val[2] > max_[2)
+        {
+          nimage.at<Vec3b>(j,i).val[0] = 0;
+          nimage.at<Vec3b>(j,i).val[1] = 0;
+          nimage.at<Vec3b>(j,i).val[2] = 0;
+        }
+
+    }
+  }
+  imshow(window_name, nimage);
+
+}
+  else
+  {
+    imshow(window_name, rawFrame);
+  }
+  
+}
+
+void updateRawFrame()
+{
+    if( cap.isOpened() )
+    {
+
+        // se a câmera ta aberta
+        cap.read(rawFrame);
+
+        //cout << "Got the frame" << endl;
+
+    }else
+    {
+        // se a câmera não ta aberta
+        cout << "Unable to get the frame" << endl;
+    }
+
+
+
+}
+
+void openCamera()
+{
+    int id = getCameraId();
+
+    cap.open(id);
+    cap.set(3,640);
+    cap.set(4,480);
+    if( cap.isOpened() )
+    {
+        // abriu a câmera
+        cout << "Camera opened succefully" << endl;
+
+    }else
+    {
+        // não conseguiu abrir a câmera
+        cout << "Unable to open the camera" << endl;
+    }
+    //CAMERA NOVA
+    cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M','J','P','G') );
+    //CAMERA NOVA
+}
+
+void closeCamera()
+{
+    cap.release();
+
+    if( cap.isOpened() )
+    {
+        // abriu a câmera
+        cout << "Unable to close the camera" << endl;
+
+    }else
+    {
+        // não conseguiu abrir a câmera
+        cout << "Camera closed succefully" << endl;
+    }
 }
 
 int main(int argc, char** argv)
 {
-	if (argc != 2) {
-		printf("usage: DisplayImage.out <Image_Path>\n");
-		return -1;
-	}
-	
-	src = imread(argv[1], 1);
-	if (!src.data) {
-		printf("No image data \n");
-		return -1;
-	}
+  char window_name[18] = "Color calibration";
+  int color_space;
+  if(argc == 1)
+  {
+    cout<<"[ERROR]: Color space not informed:"<<endl;
+    cout<<"$ ./color_calibration.exe --<color_space_value>"<<endl;
+    return 0;
+  }
+  //Flag do espaço de cor 
+  if((string)argv[1] == "--lab")
+  {
+    cout<<"Color space selected: Lab"<<endl;
+    color_space = LAB_;
+  }else if((string)argv[1] == "--hsv3b")
+  {
+    cout<<"Color space selected: HSV"<<endl;
+    color_space = HSV_;
+  }else
+  {
+    cout<<"[ERROR]: Color space not informed:"<<endl;
+    cout<<"$ ./color_calibration.exe --<color_space_value>"<<endl;
+    return 0;
+  }
 
-	namedWindow(window_name, WINDOW_AUTOSIZE);
-	imshow(window_name, src);
-	color_space = LAB_; 
+  openCamera();
+  namedWindow(window_name, WINDOW_AUTOSIZE);
 	vector<Vec3b> vet_colors;
-	setMouseCallback(window_name, onMouse, (void*)&vet_colors);
-	waitKey(0);
+  bool gettingColors = true;
+  
+  while(gettingColors)
+  {
+    cout<<"Digite a cor: Amarelo, Ciano, Rosa, Verde, Azul, Laranja, Roxo, Marrom, Vermelho, ColorEnemy1, ColorEnemy2, ColorEnemy3"<<endl;
+    cin>>current_color_name;
 
-	imshow(window_name, src);
-	waitKey(0);
+    gettingColors = false;
+    for(int i = 0; i<possibilities.size(); i++)
+    {
+      if(current_color_name == possibilities[i])
+      {
+        gettingColors = true;
+        break;
+      }
+    }
 
+    if(gettingColors == false)
+      cout<<"Nome da cor inválida: fim do programa"<<endl;
+    
+   while(gettingColors)
+    {
+        updateRawFrame();
+        for(auto it=vet_colors.begin() ; it != vet_colors.end() ; ++it) //every frame it draw the points clickeds in the image
+        {
+            circle(rawFrame, *it, 0.1, Scalar(0,0,255), 2);
+        }
+
+        imshow(window_name, rawFrame);
+        setMouseCallback(window_name, color_space == HSV_ ? onMouseHSV : onMouseLab , (void*)&points);
+      
+        if((waitKey(1) == 27)
+        {
+            break;
+        }
+    }
+  destroyWindow(window_name);
+  
+  }
+
+  closeCamera();
 	return 0;
 }
